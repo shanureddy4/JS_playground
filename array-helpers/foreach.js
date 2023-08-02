cb9cfb94c5a78be3b73695940a8ea480f53999df
bogathi reddy <bogathi.reddy@powerschool.com>
2023-08-02 17:24:01 +0530

    foreach initial commit

---
+++ array-helpers/foreach.js
@@ -0,0 +1,7 @@
+var colors = ['red','blue','green'];
+
+//here we call the function as ananymous function else in this case it is iterator function
+//forEach will pass each element in the array to the iterator function.
+colors.forEach(function(color){
+    console.log(color);
+})
